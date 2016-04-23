6a2499db8b8af564c87b2e21d69c733148b07e24
Adam Lazarus <adamlazarus92@gmail.com>
2016-04-23 23:19:02 +0800

    filled in methods in runs_controller

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,2 +1,29 @@
 class RunsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new
+    @run = current_user.runs.build
+  end
+
+  def create
+    @run = current_user.runs.build(run_params)
+    @run.user_id = current_user.id
+    if @run.save
+      redirect_to run_post_path(current_user, @run)
+    end
+  end
+
+  def show
+    @run = Run.find_by_id(params[:id])
+  end
+
+  def index
+    @user = User.find_by_id(params[:user_id])
+  end
+
+  private
+    def run_params
+      params.require(:run).permit(:time_in_minutes, :run_date, :content)
+    end
+
 end
