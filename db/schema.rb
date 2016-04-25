eba56ddebb9096ac4c19a3fb3c0f2ab65ef099c6
Adam Lazarus <adamlazarus92@gmail.com>
2016-04-25 10:34:11 +0800

    added username authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423155153) do
+ActiveRecord::Schema.define(version: 20160425022356) do
 
   create_table "runs", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,10 +36,12 @@ ActiveRecord::Schema.define(version: 20160423155153) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "run_id"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["run_id"], name: "index_users_on_run_id"
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
